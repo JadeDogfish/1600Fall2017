14e16d7288f8b844b258129975420b13abf6cdb4
Unknown <jaded.dogfish@gmail.com>
2017-08-30 11:41:41 -0600

    Changed code

---
+++ aThing/Assets/Whale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whale : Mammal {
+
+public override void Start() {
+        base.Start();
+        isBig();
+    }
+	void isBig () {
+        print(this.name + " is very big");
+    }
+}
