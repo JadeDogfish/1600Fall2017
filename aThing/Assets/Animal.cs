bc5303c9fe5179d5370d5640be791a6b7a62feee
Unknown <jaded.dogfish@gmail.com>
2017-09-01 20:58:48 -0600

    Added comment

--- aThing/Assets/Animal.cs
+++ aThing/Assets/Animal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour {
 
-	// Use this for initialization
+	// This scripting stuff is giving me a headache
 	public virtual void Start () {
 		Die();
 		Eat();
