8a83c39013e89e06a09273b080d6a3ace1e4b6df
Unknown <jaded.dogfish@gmail.com>
2017-09-01 20:42:05 -0600

    Added comments about the code

--- aThing/Assets/Bird.cs
+++ aThing/Assets/Bird.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : Animal {
-
+// Bird inherits from Animal which makes it susceptible to death
 	public override void Start() {
         base.Start();
         Fly();
+        // Bird is the only animal that flies
     }
 	void Fly () {
         print(this.name + " flies");
+        // This prints out "Bird flies"
     }
 }
