e192a4e38e32b29e3edb419f34ad8f4b357d8953
Unknown <jaded.dogfish@gmail.com>
2017-09-06 11:50:28 -0600

    Changed stuff in Unity

---
+++ aThing/Assets/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour {
+
+	// Use this for initialization
+	string myString = "Hello";
+	void Start () {
+		print(myString);
+	}
+	
+	
+}
