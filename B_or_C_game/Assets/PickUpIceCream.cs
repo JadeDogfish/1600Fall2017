9d0b5ed95bb0197a12641d87ff829d5eec02b0f2
Unknown <jaded.dogfish@gmail.com>
2017-10-29 16:30:02 -0600

    Character Movement Added

---
+++ B_or_C_game/Assets/PickUpIceCream.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpIceCream : MonoBehaviour {
+	public string[] iceCream = {"Cookies and Cream", "Vanilla", "Chocolate", "Mint", "Strawberry", "Chocolate-Vanilla Twist"};
+	public string[] toppings = {"Chocolate Syrup", "Sprinkles", "A Banana"};
+	public int[] inventory = {1,2,3,4,5,6,7};
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}	
+}
