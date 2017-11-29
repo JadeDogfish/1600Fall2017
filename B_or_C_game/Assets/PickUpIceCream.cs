884b7e8cf25115c864af931a48e16497b12d45b6
Unknown <jaded.dogfish@gmail.com>
2017-11-28 20:36:08 -0700

    Tried to Code
    I'll figure it out, don't worry

--- B_or_C_game/Assets/PickUpIceCream.cs
+++ B_or_C_game/Assets/PickUpIceCream.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PickUpIceCream : MonoBehaviour {
 	public string[] iceCream = {"Cookies and Cream", "Vanilla", "Chocolate", "Mint", "Strawberry", "Chocolate-Vanilla Twist"};
 	public string[] toppings = {"Chocolate Syrup", "Sprinkles", "A Banana"};
-	public int[] inventory = {1,2,3,4,5,6,7};
+	public List<string> inventory = new List<string>();
+	public string inCone;
 	// Use this for initialization
-	void Start () 
-	{
+	
+	public string pickUp () {
 		
-	}	
+		}	
 }
