86111a1f839a8d3ede4cad8fdfeeccc23d9aa819
Unknown <jaded.dogfish@gmail.com>
2017-11-30 20:41:49 -0700

    Trying to figure out random generation

--- B_or_C_game/Assets/Customers.cs
+++ B_or_C_game/Assets/Customers.cs
@@ -3,14 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Customers : MonoBehaviour {
-
+	
+	public string[] iceCream = {"Cookies and Cream", "Vanilla", "Chocolate", "Mint", "Strawberry", "Chocolate-Vanilla Twist"};
+	public string[] toppings = {"Chocolate Syrup", "Sprinkles", "A Banana"};
+	int choice1 = Random.Range(0, 5);
+	int choice2 = Random.Range(0, 5);
+	int choice3 = Random.Range(0, 5);
+	int choice4 = Random.Range(0, 2);
+	int choice5 = Random.Range(0, 5);	
+	string result1; string result2; string result3; string result4; int result5;
+	
 	// Use this for initialization
 	void Start () {
-		
+		int choice1 = Random.Range(0, 5);
+		int choice2 = Random.Range(0, 5);
+		int choice3 = Random.Range(0, 5);
+		int choice4 = Random.Range(0, 2);
+		int choice5 = Random.Range(0, 5);
+		int orderNum = choice5;
+		CustomerOrders();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	public void CustomerOrderChoice(){
+		//not sure how else to do it, really
+		if(choice1 == 0){
+			result1 = iceCream[0];
+		}else if(choice1 == 1){
+			result1 = iceCream[1];
+		}else if(choice1 == 2){
+			result1 = iceCream[2];
+		}else if(choice1 == 3){
+			result1 = iceCream[3];
+		}else if(choice1 == 4){
+			result1 = iceCream[4];
+		}else if(choice1 == 5){
+			result1 = iceCream[5];
+		}
+		if(choice2 == 0){
+			result2 = iceCream[0];
+		}else if(choice2 == 1){
+			result2 = iceCream[1];
+		}else if(choice2 == 2){
+			result2 = iceCream[2];
+		}else if(choice2 == 3){
+			result2 = iceCream[3];
+		}else if(choice2 == 4){
+			result2 = iceCream[4];
+		}else if(choice2 == 5){
+			result2 = iceCream[5];
+		}
+		if(choice3 == 0){
+			result3 = iceCream[0];
+		}else if(choice3 == 1){
+			result3 = iceCream[1];
+		}else if(choice3 == 2){
+			result3 = iceCream[2];
+		}else if(choice3 == 3){
+			result3 = iceCream[3];
+		}else if(choice3 == 4){
+			result3 = iceCream[4];
+		}else if(choice3 == 5){
+			result3 = iceCream[5];
+		}
+		if(choice4 == 0){
+			result4 = toppings[0];
+		}else if(choice4 == 1){
+			result4 = toppings[1];
+		}else if(choice4 == 2){
+			result4 = toppings[2];
+		}
+		
+	}
+	void CustomerOrders(){
+		
+		switch (orderNum[6]){
+			case 0:
+				CustomerOrderChoice();
+				print("I'd like a single scoop of " + result1 + ", please.");
+				break;
+			case 1:
+				CustomerOrderChoice();
+				print("I'd like a double scoop with " +result1+ " and " +result2+ " please.");
+				break;
+			case 2:
+				CustomerOrderChoice();
+				print("I'd like a triple scoop with " +result1+ ", " +result2+ ", and " +result3+ " please.");
+				break;
+			case 3:
+				CustomerOrderChoice();
+				print("I'd like a single scoop of" +result1+ " with " +result4+ " please.");
+				break;
+			case 4:
+				CustomerOrderChoice();
+				print("I'd like a double scoop with " +result1+ " and " +result2+ " with " +result4+ " please." );
+				break;
+			case 5:
+				CustomerOrderChoice();
+				print("I'd like a triple scoop with " +result1+ ", " +result2+ ", and " +result3+ " with " +result4+ " please.");
+				break;
+			
+		}
+		{
+			
+		}
 	}
 }
--- B_or_C_game/Assets/PickUpIceCream.cs
+++ B_or_C_game/Assets/PickUpIceCream.cs
@@ -5,11 +5,54 @@ using UnityEngine;
 public class PickUpIceCream : MonoBehaviour {
 	public string[] iceCream = {"Cookies and Cream", "Vanilla", "Chocolate", "Mint", "Strawberry", "Chocolate-Vanilla Twist"};
 	public string[] toppings = {"Chocolate Syrup", "Sprinkles", "A Banana"};
-	public List<string> inventory = new List<string>();
+	public string[] inventory = new string[9];
 	public string inCone;
 	// Use this for initialization
-	
-	public string pickUp () {
+	void update (){
+		for(int i = 1; i >= 0; i--){
+			PickUp();
+		}
 		
+	}
+	public void PickUp () {
+		//here's 2 switches; others are in Customers
+		switch (iceCream[7]){
+			case "Cookies and Cream":
+				inventory[0] = "Cookies and Cream";
+				break;
+			case "Vanilla":
+				inventory[1] = "Vanilla";
+				break;
+			case "Chocolate":
+				inventory[2] = "Chocolate";
+				break;
+			case "Mint":
+				inventory[3] = "Mint";
+				break;
+			case "Strawberry":
+				inventory[4] = "Strawberry";
+				break;
+			case "Chocolate-Vanilla Twist":
+				inventory[5] = "Chocolate-Vanilla Twist";
+				break;
+			case null:
+				print("You didn't scoop any ice cream.");
+				break;
+		}
+		switch (toppings[4]){
+			case "Chocolate Syrup":
+				inventory[6] = "Chocolate Syrup";
+				break;
+			case "Sprinkles":
+				inventory[7] = "Sprinkles";
+				break;
+			case "A Banana":
+				inventory[8] = "A Banana";
+				break;
+			case null:
+				print("You didn't apply any toppings.");
+				break;
+			}
+
 		}	
 }
