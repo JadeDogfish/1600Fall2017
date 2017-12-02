8b5665d56339fbf4bd6d0cf3925e5ff6a3f1641c
Unknown <jaded.dogfish@gmail.com>
2017-12-02 15:18:21 -0700

    Added Text

---
+++ B_or_C_game/Assets/Walls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour {
+
+	// Makes it possible to not run off the edge of the shop
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
