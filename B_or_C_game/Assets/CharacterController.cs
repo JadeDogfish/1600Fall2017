2fb756816f859080ec5a4694c724c613a4f426d9
Unknown <jaded.dogfish@gmail.com>
2017-12-08 15:01:34 -0700

    Rearranged some code

--- B_or_C_game/Assets/CharacterController.cs
+++ B_or_C_game/Assets/CharacterController.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-	// Controls the character, among other things
+	// Controls the character
 	public SphereCollider player;
 	public float speed = 10f;
-	private GameObject ingredient;
-	private Ingredients ingredientScript;
-	public int Money = 20;
+	
 	// Use this for initialization
 	void Start () {
-		player = this.GetComponent<SphereCollider>();
+		
 		
 	}
 	void Update () {
@@ -21,12 +19,6 @@ public class CharacterController : MonoBehaviour {
 		Vector3 move = new Vector3(moveLeftRight, 0f, moveForwardBack);
 		transform.Translate(move * Time.deltaTime * speed);
 	}
-	void OnCollisionEnter(Collision collision){
-		if(collision.gameObject.tag == "Ingredients"){
-			ingredientScript = collision.gameObject.GetComponent<Ingredients>();
-			
-				
-			}
-		}
+	
 	}
 
---
+++ B_or_C_game/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	// has inventory
+	// has health (money)
+	// interaction
+	private GameObject ingredient;
+	private Ingredients ingredientScript;
+	public int Money = 20;
+	public bool canScoop = false;
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "Ingredients"){
+			ingredientScript = collision.gameObject.GetComponent<Ingredients>();
+			if(canScoop == true && Input.GetKeyDown(KeyCode.Space)){
+					
+			}
+			}
+		}
+	
+}
