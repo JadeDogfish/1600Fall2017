6ea64753d8b412da21ce0f53249b9550f505e8e9
Unknown <jaded.dogfish@gmail.com>
2017-12-12 16:05:57 -0700

    Added scripts to components

--- B_or_C_game/Assets/CharacterController.cs
+++ B_or_C_game/Assets/CharacterController.cs
@@ -6,14 +6,15 @@ public class CharacterController : MonoBehaviour {
 	// Controls the character
 	public float speed = 10f;
 	public float scoop = Input.GetAxis("Jump");
+	float moveLeftRight = Input.GetAxis("Horizontal");
+	float moveForwardBack = Input.GetAxis("Vertical");	
 	// Use this for initialization
 	void Start () {
 		
 		
 	}
 	void Update () {
-		float moveLeftRight = Input.GetAxis("Horizontal");
-		float moveForwardBack = Input.GetAxis("Vertical");		
+			
 		Vector3 move = new Vector3(moveLeftRight, 0f, moveForwardBack);
 		transform.Translate(move * Time.deltaTime * speed);
 	}
